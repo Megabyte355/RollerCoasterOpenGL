7b1790602771e5a98d6fdf05fe336d2ad4440d2b
rcijov <rcijov@gmail.com>
2014-08-15 01:18:45 -0400

    Alien Setup

---
+++ Source/AlienModel.h
@@ -0,0 +1,38 @@
+//
+// COMP 371 Assignment Framework
+//
+// Created by Nicolas Bergeron on 8/7/14.
+//
+// Copyright (c) 2014 Concordia University. All rights reserved.
+//
+
+#pragma once
+
+#include "Model.h"
+#include "LightModel.h"
+#include <vector>
+#include "CubeModel.h"
+
+class AlienModel : public Model
+{
+public:
+	AlienModel();
+	virtual ~AlienModel();
+
+	virtual void Update(float dt);
+	virtual void Draw();
+
+	virtual void SetLightSource(LightModel * lightSource);
+
+protected:
+	virtual bool ParseLine(const std::vector<ci_string> &token);
+
+private:
+	// @TODO 5 - You may want a container for all the parts of your vehicle
+	std::vector<CubeModel*> container;
+	std::vector<CubeModel*> rotatingXAxis;
+	std::vector<CubeModel*> rotatingYAxis;
+
+	float angularSpeedXAxis;
+	float angularSpeedYAxis;
+};
