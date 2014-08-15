0b70483f7fb68783ab22a22570439c31d8a1ba9c
Alex <d3t0ur@hotmail.com>
2014-08-15 17:57:31 -0400

    Added two SunModel files and one Sun scene.

---
+++ Source/SunModel.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "SphereModel.h"
+#include "LightModel.h"
+
+class SunModel : public SphereModel
+{
+	public:
+		SunModel(glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f));
+		SunModel(Model * parent, glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f));
+		~SunModel(void);
+		virtual void Update(float dt);
+		
+	protected:
+		virtual void Init(glm::vec3 size);
+
+	private:
+		// The vertex format could be different for different types of models
+		struct Vertex
+		{
+			glm::vec3 position;
+			glm::vec3 normal;
+			glm::vec3 color;
+		};
+
+		LightModel* light;
+
+		unsigned int mVertexArrayID;
+		unsigned int mVertexBufferID;
+		unsigned int numOfVertices;
+};
+
