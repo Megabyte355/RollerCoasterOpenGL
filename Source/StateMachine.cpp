0b20b3cf90280d66dbfc286ffa30433b5ea0ad30
Gary Chang <gary.chang.355@gmail.com>
2014-08-16 12:39:17 -0400

    Added missing files.

---
+++ Source/StateMachine.cpp
@@ -0,0 +1,14 @@
+// Contributors:
+//      Gary Chang
+
+#include "StateMachine.h"
+#include <iostream>
+
+StateMachine::StateMachine(Model* m)
+{
+    mModel = m;
+}
+
+StateMachine::~StateMachine()
+{
+}
