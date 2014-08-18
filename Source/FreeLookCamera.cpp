813123730d22f07a444f2f5dcb648b7b02843445
Gary Chang <gary.chang.355@gmail.com>
2014-08-18 02:03:42 -0400

    FreeLookCamera now calls RayCast::IntersectBoundingBoxes for collision test.

--- Source/FreeLookCamera.cpp
+++ Source/FreeLookCamera.cpp
@@ -88,94 +88,10 @@ void FreeLookCamera::Update(float dt)
     }
     if (glfwGetKey(EventManager::GetWindow(), GLFW_KEY_SPACE) == GLFW_PRESS)
     {
-		//std::vector<Model *> models = *World::GetModelsPtr();
-
-		//for (int i = 0; i < models.size(); i++)
-		//{
-		//	Model* model = models.at(i);
-		//	BoundingBox::BoundingCube boundingCube = model->boundingBox->CalculateBoundingBoxCoordinates();
-
-		//	if (boundingCube.frontFace.size() == 0)
-		//		continue;
-		//	std::cout << "=================================" << std::endl;
-		//	std::cout << model->mName.c_str() << std::endl;
-		//	std::cout << "=================================" << std::endl;
-
-		//	std::cout << "Back face" << std::endl;
-		//	vec3 p1 = boundingCube.backFace[0][0];
-		//	vec3 p2 = boundingCube.backFace[0][1];
-		//	vec3 p3 = boundingCube.backFace[0][2];
-		//	int testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	p1 = boundingCube.backFace[1][0];
-		//	p2 = boundingCube.backFace[1][1];
-		//	p3 = boundingCube.backFace[1][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	std::cout << "Front face" << std::endl;
-		//	p1 = boundingCube.frontFace[0][0];
-		//	p2 = boundingCube.frontFace[0][1];
-		//	p3 = boundingCube.frontFace[0][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	p1 = boundingCube.frontFace[1][0];
-		//	p2 = boundingCube.frontFace[1][1];
-		//	p3 = boundingCube.frontFace[1][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	std::cout << "Left face" << std::endl;
-		//	p1 = boundingCube.leftFace[0][0];
-		//	p2 = boundingCube.leftFace[0][1];
-		//	p3 = boundingCube.leftFace[0][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	p1 = boundingCube.leftFace[1][0];
-		//	p2 = boundingCube.leftFace[1][1];
-		//	p3 = boundingCube.leftFace[1][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	std::cout << "Right face" << std::endl;
-		//	p1 = boundingCube.rightFace[0][0];
-		//	p2 = boundingCube.rightFace[0][1];
-		//	p3 = boundingCube.rightFace[0][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	p1 = boundingCube.rightFace[1][0];
-		//	p2 = boundingCube.rightFace[1][1];
-		//	p3 = boundingCube.rightFace[1][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	std::cout << "Top face" << std::endl;
-		//	p1 = boundingCube.topFace[0][0];
-		//	p2 = boundingCube.topFace[0][1];
-		//	p3 = boundingCube.topFace[0][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	p1 = boundingCube.topFace[1][0];
-		//	p2 = boundingCube.topFace[1][1];
-		//	p3 = boundingCube.topFace[1][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	std::cout << "Down face" << std::endl;
-		//	p1 = boundingCube.downFace[0][0];
-		//	p2 = boundingCube.downFace[0][1];
-		//	p3 = boundingCube.downFace[0][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-
-		//	p1 = boundingCube.downFace[1][0];
-		//	p2 = boundingCube.downFace[1][1];
-		//	p3 = boundingCube.downFace[1][2];
-		//	testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-		//}
-
-        ////vec4 originPoint, vec4 directionVector, vec4 p1, vec4 p2, vec4 p3
-        //std::vector<Model::Vertex> tempVec = (*World::GetModelsPtr())[2]->GetWorldVertices();
-        //vec3 p1 = tempVec[0].position;
-        //vec3 p2 = tempVec[1].position;
-        //vec3 p3 = tempVec[2].position;
-
-        //int testResult = RayCast::Intersect3DTriangle(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
-        //int distanceResult = RayCast::LinePlaneIntersection(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f), p1, p2, p3);
+        RayCast::CollisionResult collision = RayCast::IntersectBoundingBoxes(vec4(mPosition, 1.0f), vec4(mLookAt, 1.0f));
+        std::cout << "Collision point x: " << collision.collisionPointWorld.x << std::endl;
+        std::cout << "Collision point y: " << collision.collisionPointWorld.y << std::endl;
+        std::cout << "Collision point z: " << collision.collisionPointWorld.z << std::endl << std::endl;
     }
 }
 
