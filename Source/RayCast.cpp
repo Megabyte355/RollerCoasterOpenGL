18bdfecf0aca037682d07023c489081a88e8b2ec
Gary Chang <gary.chang.355@gmail.com>
2014-08-18 01:54:48 -0400

    Moved collision testing code from FreeLookCamera to RayCast class.

--- Source/RayCast.cpp
+++ Source/RayCast.cpp
@@ -5,35 +5,201 @@
 
 #include <GLM/glm.hpp>
 #include "RayCast.h"
-
+#include "World.h"
+#include "BoundingBox.h"
 
 RayCast::RayCast()
 {
+}
+
+RayCast::CollisionResult RayCast::IntersectBoundingBoxes(vec4 originPoint, vec4 directionVector)
+{
+    std::vector<Model *> models = *World::GetModelsPtr();
+    std::vector<CollisionResult> collisionResults;
+
+    for (int i = 0; i < models.size(); i++)
+    {
+    	Model* model = models.at(i);
+    	BoundingBox::BoundingCube boundingCube = model->boundingBox->CalculateBoundingBoxCoordinates();
+        CollisionResult currentResult;
+
+        if (boundingCube.frontFace.size() == 0)
+        {
+            continue;
+        }
+
+        std::cout << "=================================" << std::endl;
+        std::cout << model->mName.c_str() << std::endl;
+        std::cout << "=================================" << std::endl;
+
+        std::cout << "Back face" << std::endl;
+        vec3 p1 = boundingCube.backFace[0][0];
+        vec3 p2 = boundingCube.backFace[0][1];
+        vec3 p3 = boundingCube.backFace[0][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        p1 = boundingCube.backFace[1][0];
+        p2 = boundingCube.backFace[1][1];
+        p3 = boundingCube.backFace[1][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        std::cout << "Front face" << std::endl;
+        p1 = boundingCube.frontFace[0][0];
+        p2 = boundingCube.frontFace[0][1];
+        p3 = boundingCube.frontFace[0][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        p1 = boundingCube.frontFace[1][0];
+        p2 = boundingCube.frontFace[1][1];
+        p3 = boundingCube.frontFace[1][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        std::cout << "Left face" << std::endl;
+        p1 = boundingCube.leftFace[0][0];
+        p2 = boundingCube.leftFace[0][1];
+        p3 = boundingCube.leftFace[0][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        p1 = boundingCube.leftFace[1][0];
+        p2 = boundingCube.leftFace[1][1];
+        p3 = boundingCube.leftFace[1][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        std::cout << "Right face" << std::endl;
+        p1 = boundingCube.rightFace[0][0];
+        p2 = boundingCube.rightFace[0][1];
+        p3 = boundingCube.rightFace[0][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        p1 = boundingCube.rightFace[1][0];
+        p2 = boundingCube.rightFace[1][1];
+        p3 = boundingCube.rightFace[1][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        std::cout << "Top face" << std::endl;
+        p1 = boundingCube.topFace[0][0];
+        p2 = boundingCube.topFace[0][1];
+        p3 = boundingCube.topFace[0][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        p1 = boundingCube.topFace[1][0];
+        p2 = boundingCube.topFace[1][1];
+        p3 = boundingCube.topFace[1][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        std::cout << "Down face" << std::endl;
+        p1 = boundingCube.downFace[0][0];
+        p2 = boundingCube.downFace[0][1];
+        p3 = boundingCube.downFace[0][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+
+        p1 = boundingCube.downFace[1][0];
+        p2 = boundingCube.downFace[1][1];
+        p3 = boundingCube.downFace[1][2];
+        currentResult = RayCast::Intersect3DTriangle(originPoint, directionVector, p1, p2, p3, model);
+        if (currentResult.collision)
+        {
+            collisionResults.push_back(currentResult);
+        }
+    }
+
+    if (collisionResults.size() > 0)
+    {
+        CollisionResult closestCollision = collisionResults.back();
+        float minDistance = length(closestCollision.collisionPointWorld - originPoint);
+        
+        for each(CollisionResult c in collisionResults)
+        {
+            float currentDistance = length(c.collisionPointWorld - originPoint) ;
+            if (currentDistance < minDistance)
+            {
+                closestCollision = c;
+                minDistance = currentDistance;
+            }
+        }
+        return closestCollision;
+    }
+
+    // No collision
+    return
+    {
+        glm::vec4(0.0f, 0.0f, 0.0f, 0.0f),
+        nullptr,
+        glm::vec4(0.0f, 0.0f, 0.0f, 0.0f),
+        false
+    };
 }
 
 // intersect3D_SegmentPlane(): intersect a segment and a plane
 //    Input:  originPoint = a point in model, directionVector = vector pointing at triangle, p1/p2/p3 = points that create 3D triangle
 //    Return: 0 = disjoint (no intersection)
 //            1 = intersection in the unique point intersectionPoint
-int RayCast::Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1, vec3 p2, vec3 p3) {
+RayCast::CollisionResult RayCast::Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1, vec3 p2, vec3 p3, Model* model) {
 
-    //std::cout << "p1 {" << p1.x << " " << p1.y << " " << p1.z << "}" << std::endl;
-    //std::cout << "p2 {" << p2.x << " " << p2.y << " " << p2.z << "}" << std::endl;
-    //std::cout << "p3 {" << p3.x << " " << p3.y << " " << p3.z << "}" << std::endl;
+    CollisionResult cResult =
+    {
+        glm::vec4(0.0f, 0.0f, 0.0f, 0.0f),
+        nullptr,
+        glm::vec4(0.0f, 0.0f, 0.0f, 0.0f),
+        false
+    };
 
     vec4 w = originPoint - vec4(p1, 1.0f);
 
     vec3 p1p2Vector = p2 - p1;
     vec3 p1p3Vector = p3 - p1;
     vec4 normal = vec4(glm::cross(p1p2Vector, p1p3Vector), 0.0f);
-    //std::cout << "normal {" << normal.x << " " << normal.y << " " << normal.z << "}" << std::endl;
+    
     vec4 normalizedNormal = normalize(normal);
-    //std::cout << "normalizedNormal {" << normalizedNormal.x << " " << normalizedNormal.y << " " << normalizedNormal.z << "}" << std::endl;
 
     // Calculate dot product is 90 < theta < 270  --> greater than 0
     if (glm::dot(normalizedNormal, glm::normalize(directionVector)) > 0.0f) {
         std::cout << "NOT FACING US!!!" << std::endl;
-        return 0;
+        return cResult;
     }
 
     float numerator = -glm::dot(normalizedNormal, w);
@@ -41,7 +207,7 @@ int RayCast::Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1
 
     // Line is parallel to plane
     if (fabs(denominator) < 0.0001f ) {           
-        return false;
+        return cResult;
     }
     //Continue to compute intersecting point
     float t = numerator / denominator;
@@ -49,7 +215,7 @@ int RayCast::Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1
     
     // no intersection
     if (t < 0) {
-        return 0;
+        return cResult;
     }
     
     //Finding resulting point on plane: P = originPoint + tv
@@ -77,11 +243,17 @@ int RayCast::Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1
 
     if (fabs(totalArea - originalTriangleArea) < 0.001f) {
         std::cout << "SUPERU COLLISIONURU" << std::endl << std::endl;
-        return 1;
+        return
+        {
+            intersectionPoint,
+            model,
+            normalizedNormal,
+            true
+        };
     }
 
     std::cout << "NOT COLLIDING!!!" << std::endl;
-    return 0;    
+    return cResult;
 }
 
 RayCast::~RayCast()
--- Source/RayCast.h
+++ Source/RayCast.h
@@ -7,17 +7,18 @@ using namespace glm;
 class RayCast
 {
 public:
-	RayCast();
-    static int Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1, vec3 p2, vec3 p3);
+    struct CollisionResult
+    {
+        vec4 collisionPointWorld; //coordinates in world space where the collision actually occurred(good to know, I guess)
+        Model * model; //the model being hit by the collision
+        vec4 normal; //the normal vector of the triangle being collided(this is SUPER IMPORTANT for decals)
+        bool collision;
+    };
+    
+    RayCast();
+    static CollisionResult IntersectBoundingBoxes(vec4 originPoint, vec4 directionVector);
+    static CollisionResult Intersect3DTriangle(vec4 originPoint, vec4 directionVector, vec3 p1, vec3 p2, vec3 p3, Model* model);
 	~RayCast();
-
-	struct CollisionResult
-	{
-		vec4 collisionPointWorld; //coordinates in world space where the collision actually occurred(good to know, I guess)
-		vec4 collisionPointModel; //coordinates relative to the model where it's being hit (useful for decals)
-		Model * model; //the model being hit by the collision
-		vec4 normal; //the normal vector of the triangle being collided(this is SUPER IMPORTANT for decals)
-	};
 
 private:
 	
